4f8a2a64a1b096e576b2e3fc8fdf5a462b0a3c62
Hazle <xendyex@tutanota.com>
2024-07-15 21:03:01 +0100

    you

---
+++ LiveTests/alumina.py
@@ -0,0 +1,11 @@
+#
+#
+# Yes, Alumina is one python file.
+
+
+def init(name madeWith=True):
+  print(" ")
+  if madeWith != False:
+    print("Made with Alumina, by hablethedev")
+    print(" ")
+  print("Welcome to " + name + ".")
