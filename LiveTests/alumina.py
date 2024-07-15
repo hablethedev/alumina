539499c42ebf99f887a0f1c2c3cf603c456c7634
Hazle <xendyex@tutanota.com>
2024-07-15 21:17:08 +0100

    Add handling for the map a bit

--- LiveTests/alumina.py
+++ LiveTests/alumina.py
@@ -2,9 +2,14 @@
 #
 # Yes, Alumina is one python file.
 
-def init(name, madeWith=True):
+def init(name, madeWith=True, mapL, mapE):
+  global mapLocations
+  mapLocations = mapL
+  global mapEntities
+  mapEntities = mapE
   print(" ")
   if madeWith != False:
     print("Made with Alumina, by hablethedev")
     print(" ")
   print("Welcome to " + name + ".")
+  print("Currently in " + mapLocations[mapEntities.value("P")])
